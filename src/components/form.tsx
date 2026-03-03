ed78065df0e357b927742931940917a1d111b955
weslleyqueiroz <weslleyqueiroz773@gmail.com>
2026-03-02 22:26:40 -0300

    alterando a pagina de login. Adicionando imagem e botoes de google e facebook

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,27 +1,49 @@
-import { Button } from "@react-navigation/elements"
-import {TextInput, Text, View, Pressable} from "react-native"
-import {Ionicons, Feather} from "@expo/vector-icons"
+    import { Button } from "@react-navigation/elements"
+    import {TextInput, Text, View, Pressable, Image} from "react-native"
+    import {Ionicons, Feather} from "@expo/vector-icons"
+    import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+    import AntDesign from '@expo/vector-icons/AntDesign';
+    import { useState } from "react";
+    import { GoogleSignin, isSuccessResponse, User } from '@react-native-google-signin/google-signin';
 
 
-export function Register (){
-    return(
-
-        <View className="flex-1 pt-16 px-4 bg-white">
-            <View className=" h-12 flex-row items-center w-full bg-slate-300 rounded-full px-4">
-                <Ionicons name="search" size={30} color="#6B8DF2"/>
-           
-     
-            <TextInput className=" flex-1 h-full ml-2 p-0 rounded-full" 
-             placeholder="Procure uma área..."
-             /> 
-
-            </View> 
-        </View>
+    export function Register (){
 
 
+        return(
+
+            <View>
+                    <Image className="h-48 w-48 items-center mt-20" source={require('../../assets/images/logo.jpg')}></Image>
+                    <View className=" h-10 flex-row items-center w-30 bg-slate-300 rounded-full px-4 mt-20">
+                    <AntDesign name="google" size={20} color="#6B8DF2"/>
+            
+        
+                <TextInput className=" flex-1 h-full ml-1 p-0 rounded-full" 
+                placeholder="Faça login com o google"
+                /> 
+                </View> 
+
+                <View>
+                <View className=" h-10 flex-row items-center w-30 bg-slate-300 rounded-full px-4 mt-20">
+                    <FontAwesome5 name="facebook" size={20} color="#6B8DF2"/>
+            
+        
+                <TextInput className=" flex-1 h-full ml-1 p-0 rounded-full" 
+                placeholder="Faça login com o facebook"
+                /> 
+                </View> 
+            </View>
+
+            </View>
+
+
+
+            
 
 
 
 
-    )
-}
+
+
+        )
+    }
